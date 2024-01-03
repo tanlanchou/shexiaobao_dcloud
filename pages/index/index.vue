d4d70264553b3346fa83046be8551f903d445aba
zhangjielong <jielong.zhang@apuqi.com>
2024-01-03 18:12:44 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+			
+			<uni-steps :options="[{title: '事件一'}, {title: '事件二'}, {title: '事件三'}, {title: '事件四'}]" :active="1"></uni-steps>
+						 
+		</view>
+	</view>
+</template>
+
+<script>
+	import { tokenKey } from "@/common/enum.js"
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onReady() {
+			debugger;
+			const value = uni.getStorageSync(tokenKey);
+			if(!value || value == '') {
+					uni.redirectTo({
+						url: "login"
+					})
+			}
+			
+		},
+		onLoad() {
+			
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/outer/login.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="test">
+		这是的login页面
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
